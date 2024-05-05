de912a8cedfdd82c8529697cb8fdcf55fb12071e
Jason Jones <jsjones96@gmail.com>
2024-05-05 10:43:19 -0700

    feat: add svg asset

--- packages/app/src/components/app.js
+++ packages/app/src/components/app.js
@@ -4,8 +4,10 @@ class App extends HTMLElement {
     }
 
     connectedCallback() {
-        this.innerHTML =
-            '<h1>Hello Web Platform!</h1><h2>Rendered from the webapp component with light DOM</h2>';
+        this.innerHTML = `<h1>Hello Web Platform!</h1>
+            <h2>Rendered from the webapp component with light DOM</h2>
+            <img class="image-medium" src="assets/api.svg" alt="api graphic"/>
+            `;
     }
 }
 
